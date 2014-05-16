cfa65b8a070a265c4718b627a15e57bdc7d1d923
Kenneth Endfinger <kaendfinger@gmail.com>
2014-05-16 14:41:09 -0400

    Created metadata.groovy

---
+++ meta/metadata.groovy
@@ -0,0 +1,52 @@
+/* Language List */
+languages = [
+    [
+        name: "Groovy",
+        extensions: [
+            ".groovy",
+            ".gvy"
+        ]
+    ],
+    [
+        name: "Java",
+        extensions: [
+            ".java"    
+        ]
+    ],
+    [
+        name: "JavaScript",
+        extensions: [
+            ".js"    
+        ]
+    ],
+    [
+        name: "Scala",
+        extensions: [
+            ".scala"    
+        ]
+    ],
+    [
+        name: "Ruby",
+        extensions: [
+            ".rb"    
+        ]
+    ],
+    [
+        name: "Python",
+        extensions: [
+            ".py"    
+        ]
+    ],
+    [
+        name: "JSON",
+        extensions: [
+            ".json"    
+        ]
+    ],
+    [
+        name: "CoffeeScript",
+        extensions: [
+            ".coffee"    
+        ]
+    ]
+]
