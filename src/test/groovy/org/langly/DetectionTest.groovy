13a2248d6a388e441d9cfb30418a0d2f6a15301e
Kenneth Endfinger <kaendfinger@gmail.com>
2014-05-17 10:52:35 -0400

    Add JavaScript Detection Test

--- src/test/groovy/org/langly/DetectionTest.groovy
+++ src/test/groovy/org/langly/DetectionTest.groovy
@@ -6,16 +6,23 @@ import static org.junit.Assert.*
 
 class DetectionTest {
     Langly langly
-    
+
     @Before
     void before() {
         this.langly = new Langly()
     }
-    
+
     @Test
     void testGroovyDetection() {
         assertTrue(langly.detect(new CodeFile("test.groovy", """
             println "Hello World"
         """.stripIndent())) == langly.language("Groovy"))
+    }
+
+    @Test
+    void testJavaScriptDetection() {
+        assertTrue(langly.detect(new CodeFile("test.js", """
+            console.log("Hello World");
+        """.stripIndent())) == langly.language("JavaScript"))
     }
 }
