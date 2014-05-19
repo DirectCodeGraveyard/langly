631549d85ef8d9edb4b4d2a5bc5f28c96132e585
Kenneth Endfinger <kaendfinger@gmail.com>
2014-05-19 09:56:52 -0400

    Detection Test Fix

--- src/test/groovy/org/langly/DetectionTest.groovy
+++ src/test/groovy/org/langly/DetectionTest.groovy
@@ -10,7 +10,7 @@ class DetectionTest {
            assert Language[langDir.name] != null, "Sample Directory '${langDir.name}' could not be matched to a language"
            langDir.eachFile { file ->
                def blob = new FileBlob(file)
-               assert blob.language() == Language[langDir.name], "'${file.name}' with language '${lang.name}' was not able to be detected"
+               assert blob.language() == Language[langDir.name], "'${file.name}' with language '${langDir.name}' was not able to be detected"
            }
         }
     }
