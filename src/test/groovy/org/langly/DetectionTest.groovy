6330aae29113e806374685c8d0bd22c513dba9ba
Kenneth Endfinger <kaendfinger@gmail.com>
2014-05-18 20:56:08 -0400

    Tests: Fix Possible incorrectly Passing Test by asserting that a sample directory's language exists

--- src/test/groovy/org/langly/DetectionTest.groovy
+++ src/test/groovy/org/langly/DetectionTest.groovy
@@ -7,6 +7,7 @@ class DetectionTest {
     void testSamplesDetection() {
         def samplesDir = new File("data/samples")
         samplesDir.eachFile { langDir ->
+           assert Language[langDir.name] != null, "Sample Directory '${langDir.name}' could not be matched to a language"
            langDir.eachFile { file ->
                def blob = new FileBlob(file)
                assert blob.language() == Language[langDir.name], "'${file.name}' with language '${lang.name}' was not able to be detected"
